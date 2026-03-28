b2ba4724717a94a967ff6928ba2c91e03aeaf04a
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-03-28 19:05:30 +0000

    Fix Betty documentation for struct

--- sort.h
+++ sort.h
@@ -1,11 +1,8 @@
-#ifndef SORT_H
-#define SORT_H
-
-#include <stddef.h>
-#include <stdio.h>
-
 /**
  * struct listint_s - Doubly linked list node
+ * @n: integer stored in the node
+ * @prev: points to the previous node
+ * @next: points to the next node
  */
 typedef struct listint_s
 {
@@ -13,12 +10,3 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-
-#endif
